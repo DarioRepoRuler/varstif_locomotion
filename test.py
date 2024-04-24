f5f1d9218ebc6eb679ba7e672181ae0af6ff6615
DarioRepoRuler <e11806417@student.tuwien.ac.at>
2024-04-24 11:24:17 +0200

    Fixed test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import hydra
 from omegaconf import DictConfig
 from pathlib import Path
-from envs.robots.bruce import BruceEnv
+from envs.robots.go2_env import GO2Env
 from envs.common.wrapper.torch_wrapper import TorchWrapper
 from envs.common.wrapper.render_wrapper import RenderWrapper
 from envs.common.wrapper.training_wrapper import VmapWrapper, AutoResetWrapper
@@ -11,7 +11,7 @@ from tasks.PPOTaskBase import PPOTaskBase
 
 @hydra.main(config_path='config', config_name='test', version_base="1.2")
 def test(cfg: DictConfig):
-    env = BruceEnv(cfg.env)
+    env = GO2Env(cfg.env)
     env = VmapWrapper(env, batch_size=1)
     env = AutoResetWrapper(env)
     env = TorchWrapper(env, device='cuda:0', backend='gpu')
